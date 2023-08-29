a04e1294f97e9714276453825cdf173d3140c8ae
Олег <olegkovalets1112003@knu.ua>
2023-08-30 00:07:24 +0300

    Add styles

--- src/js/render-forecast.js
+++ src/js/render-forecast.js
@@ -3,14 +3,20 @@ function renderForecast(data, city) {
     .map(
       item => `
       <div class="card">
-        <img src="${item.day.condition.icon}" alt="${
+        <div class="thumb">
+            <img src="${item.day.condition.icon}" alt="${
         item.day.condition.text
       }" />
-        <h2>${city.toUpperCase()}</h2>
-        <p>Date: ${item.date}&#8451;</p>
-        <p>Temperature: ${item.day.maxtemp_c}&#8451;</p>
-        <p>Max temperature: ${item.day.maxtemp_c}&#8451;</p>
-        <p>Min temperature: ${item.day.mintemp_c}&#8451;</p>
+        </div>
+        <div class="card-info">
+            <h2 class="city">${city.toUpperCase()}</h2>
+            <p class="date">${item.date}</p>
+            <p class="temp">${item.day.maxtemp_c}&#8451;</p>
+            <div class="min-max-temp">
+              <p class="min-temp">Min: ${item.day.mintemp_c}&#8451;</p>
+              <p class="max-temp">Max: ${item.day.maxtemp_c}&#8451;</p>
+            </div>
+        </div>
       </div>
         `
     )
