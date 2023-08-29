9be0de157c6cfd961c1107d4d0107e8e95e3be0b
Oleggman <92540540+Oleggman@users.noreply.github.com>
2023-08-29 13:07:01 +0000

    Deploying to gh-pages from @ Oleggman/Weather-service@6ad777712fe4d3fad5fba4964c7a5273cf38a032 🚀

--- index.3674867a.js
+++
@@ -1,2 +0,0 @@
-!function(){function e(e){var t=new URLSearchParams({key:"204c4f35cbd541579f7122042232908",q:e,days:14});return fetch("".concat("http://api.weatherapi.com/v1/forecast.json","?").concat(t)).then((function(e){if(!e.ok)throw new Error(e.statusText);return e.json()}))}({form:document.querySelector(".forecast-form")}).form.addEventListener("submit",(function(t){t.preventDefault(),e(t.currentTarget.elements.city.value).then(console.log).catch(console.log).finally(t.target.reset())}))}();
-//# sourceMappingURL=index.3674867a.js.map
--- index.f9fc6d34.js
+++
@@ -1,2 +0,0 @@
-function e(e){const t=new URLSearchParams({key:"204c4f35cbd541579f7122042232908",q:e,days:14});return fetch(`http://api.weatherapi.com/v1/forecast.json?${t}`).then((e=>{if(!e.ok)throw new Error(e.statusText);return e.json()}))}({form:document.querySelector(".forecast-form")}).form.addEventListener("submit",(function(t){t.preventDefault();e(t.currentTarget.elements.city.value).then(console.log).catch(console.log).finally(t.target.reset())}));
-//# sourceMappingURL=index.f9fc6d34.js.map
