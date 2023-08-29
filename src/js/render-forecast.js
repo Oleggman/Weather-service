b1c8580a39b36c88c4345ea864e0de636403b957
Олег <olegkovalets1112003@knu.ua>
2023-08-29 17:04:44 +0300

    Update some features

--- src/js/render-forecast.js
+++ src/js/render-forecast.js
@@ -1,11 +1,12 @@
 function renderForecast(data, city) {
-  console.log(data);
   return data
     .map(
       item => `
       <div class="card">
-        <img src="${item.day.condition.icon}" alt="${item.day.condition.text}" />
-        <h2>${city}</h2>
+        <img src="${item.day.condition.icon}" alt="${
+        item.day.condition.text
+      }" />
+        <h2>${city.toUpperCase()}</h2>
         <p>Date: ${item.date}&#8451;</p>
         <p>Temperature: ${item.day.maxtemp_c}&#8451;</p>
         <p>Max temperature: ${item.day.maxtemp_c}&#8451;</p>
