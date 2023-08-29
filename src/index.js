d13a388c36e90e024dbcb8adef069fb769b0bff0
Олег <olegkovalets1112003@knu.ua>
2023-08-29 16:04:12 +0300

    Add service-forecast.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { serviceForecast } from './js/service-forecast';
+
 const refs = {
   form: document.querySelector('.forecast-form'),
 };
@@ -9,14 +11,10 @@ const API_KEY = '204c4f35cbd541579f7122042232908';
 
 function onSearchForecast(evt) {
   evt.preventDefault();
+  const city = evt.currentTarget.elements.city.value;
 
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: evt.currentTarget.elements.city.value,
-    days: 14,
-  });
-
-  fetch(`${API_URL}?${params}`)
-    .then(response => response.json())
-    .then(console.log);
+  serviceForecast(city)
+    .then(console.log)
+    .catch(console.log)
+    .finally(evt.target.reset());
 }
---
+++ src/js/service-forecast.js
@@ -0,0 +1,20 @@
+const API_URL = 'http://api.weatherapi.com/v1/forecast.json';
+const API_KEY = '204c4f35cbd541579f7122042232908';
+
+function serviceForecast(city) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: city,
+    days: 14,
+  });
+
+  return fetch(`${API_URL}?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
+  });
+}
+
+export { serviceForecast };
