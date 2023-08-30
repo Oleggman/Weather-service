f011992337e1ea9da323bbdc866dacbc8ce3852c
Олег <olegkovalets1112003@knu.ua>
2023-08-30 22:57:21 +0300

    Fixed api url (http to https)

--- src/js/service-forecast.js
+++ src/js/service-forecast.js
@@ -1,6 +1,6 @@
 import Notiflix from 'notiflix';
 
-const API_URL = 'http://api.weatherapi.com/v1/forecast.json';
+const API_URL = 'https://api.weatherapi.com/v1/forecast.json';
 const API_KEY = '204c4f35cbd541579f7122042232908';
 
 function serviceForecast(city) {
